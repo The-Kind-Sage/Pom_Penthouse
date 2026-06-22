b535e00abbcd942e58b38c2e4644564ff7b694be
Rohit Chand <119827556+The-Kind-Sage@users.noreply.github.com>
2026-06-22 19:40:02 +0545

    build done

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DMGn0bdZ.mjs
@@ -0,0 +1,117 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DMGn0bdZ.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/admin",
+			"/api/bookings",
+			"/api/contact",
+			"/api/upload"
+		],
+		preloads: [
+			"/assets/index-BTWsUpAA.js",
+			"/assets/preload-helper-h0ErIP5y.js",
+			"/assets/jsx-runtime-_i1yuJNt.js"
+		],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-BTWsUpAA.js"
+		} }]
+	},
+	"/": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/index.tsx",
+		children: void 0,
+		css: ["/assets/routes-BxgC7ZK2.css"],
+		preloads: [
+			"/assets/routes-CS_a9vjT.js",
+			"/assets/createLucideIcon-D5IPYcvY.js",
+			"/assets/sun-B0eIMY8c.js",
+			"/assets/plus--Swd9Piz.js",
+			"/assets/x-R_9MrcCE.js",
+			"/assets/dist-C1THZyBm.js"
+		]
+	},
+	"/admin": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.tsx",
+		children: [
+			"/admin/analytics",
+			"/admin/bookings",
+			"/admin/login",
+			"/admin/penthouses",
+			"/admin/settings",
+			"/admin/users",
+			"/admin/"
+		],
+		preloads: [
+			"/assets/admin-BysFKlyB.js",
+			"/assets/createLucideIcon-D5IPYcvY.js",
+			"/assets/sun-B0eIMY8c.js",
+			"/assets/calendar-check-Cbn3PViL.js",
+			"/assets/search-DJJrh6Ol.js",
+			"/assets/admin-store-DF62HLOk.js"
+		]
+	},
+	"/admin/analytics": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.analytics.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/admin.analytics-CrcRuLgN.js",
+			"/assets/hooks-BuHyL4nj.js",
+			"/assets/clsx-CjueKrWZ.js"
+		]
+	},
+	"/admin/bookings": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.bookings.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/admin.bookings-Bx0U8Mj7.js",
+			"/assets/hooks-BuHyL4nj.js",
+			"/assets/x-R_9MrcCE.js",
+			"/assets/dist-C1THZyBm.js"
+		]
+	},
+	"/admin/login": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.login.tsx",
+		children: void 0,
+		preloads: ["/assets/admin.login-BbW_AvfV.js"]
+	},
+	"/admin/penthouses": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.penthouses.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/admin.penthouses-DWJFtz0q.js",
+			"/assets/hooks-BuHyL4nj.js",
+			"/assets/plus--Swd9Piz.js",
+			"/assets/x-R_9MrcCE.js",
+			"/assets/dist-C1THZyBm.js"
+		]
+	},
+	"/admin/settings": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.settings.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/admin.settings-C5C_m_MH.js",
+			"/assets/hooks-BuHyL4nj.js",
+			"/assets/clsx-CjueKrWZ.js",
+			"/assets/dist-C1THZyBm.js"
+		]
+	},
+	"/admin/users": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.users.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/admin.users-Df_jdLgb.js",
+			"/assets/hooks-BuHyL4nj.js",
+			"/assets/dist-C1THZyBm.js"
+		]
+	},
+	"/admin/": {
+		filePath: "D:/DrillThru Projects/pom-penthouse/src/routes/admin.index.tsx",
+		children: void 0,
+		preloads: ["/assets/admin.index-Ch2elO3n.js", "/assets/hooks-BuHyL4nj.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
